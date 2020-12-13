41879de36cbec0ad1a801e55aeff46477ed79726
georgeiliadis91 <george.iliadis.91@gmail.com>
2020-12-13 18:57:10 +0200

    Finalized ym first kotlin app

---
+++ app/src/main/java/com/example/first_app/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.first_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.todolist.TodoAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var todoAdapter:TodoAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        todoAdapter= TodoAdapter(mutableListOf())
+
+        rvTodoItems.adapter = todoAdapter
+        rvTodoItems.layoutManager = LinearLayoutManager(this)
+
+
+
+        btnAddTodo.setOnClickListener {
+            val todoTitle = etTodoTitle.text.toString()
+            if(todoTitle.isNotEmpty()) {
+                val todo = Todo(todoTitle)
+                todoAdapter.addTodo(todo)
+                etTodoTitle.text.clear()
+            }
+        }
+
+        btnDeleteTodo.setOnClickListener {
+            todoAdapter.deleteDoneTodos()
+        }
+    }
+}
